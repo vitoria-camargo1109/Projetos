2c6139475708c3012bf73033d8538ede3de4dbd2
vitoria-camargo1109 <vitoriafreirecamargo29@gmail.com>
2026-05-28 20:05:27 -0300

    Adição de While nos códigos.

--- lista_2/classificacaoDeTriangulos.py
+++ lista_2/classificacaoDeTriangulos.py
@@ -2,13 +2,47 @@
 Equilátero (3 lados iguais), Isósceles (2 lados iguais)
 ou Escaleno (todos diferentes)."""
 
-l1 = float(input("Digite o valor do primeiro lado do triângulo: "))
-l2 = float(input("Digite o valor do segundo lado do triângulo: "))
-l3 = float(input("Digite o valor do terceiro lado do triângulo: "))
+start = True
+start1 = True
 
-if l1 == l2 and l2 == l3:
-    print("É um triângulo Equilátero!")
-elif l1 == l2 or l2 == l3 or l3 == l1:
-    print("É um triângulo Isósceles!")
-else:
-    print("É um triângulo Escaleno!")
+while start:
+
+    l1 = float(input("Digite o valor do primeiro lado do triângulo: "))
+    l2 = float(input("Digite o valor do segundo lado do triângulo: "))
+    l3 = float(input("Digite o valor do terceiro lado do triângulo: "))
+
+    if l1 == l2 and l2 == l3:
+        print("É um triângulo Equilátero!")
+    elif l1 == l2 or l2 == l3 or l3 == l1:
+        print("É um triângulo Isósceles!")
+    else:
+        print("É um triângulo Escaleno!")
+
+    while True:
+
+        print()
+        print("Deseja adicionar outros valores?")
+        print()
+        print("-" * 30)
+        print()
+        print("Deseja continuar?")
+        print("1- Sim\n"
+              "2- Não")
+        escolha = int(input("Escolha: "))
+
+        while True:
+            if escolha != 1 and escolha != 2:
+                print("Opção inválida, Tente novamente!")
+                escolha = int(input("Sim ou não?: "))
+                break
+            else:
+                break
+
+        if escolha == 1:
+            break
+        elif escolha == 2:
+            start = False
+            break
+
+
+print("Volte sempre!")
--- lista_2/gerenciadorDePagamentos.py
+++ lista_2/gerenciadorDePagamentos.py
@@ -6,30 +6,59 @@ considerando o seu preço normal e condição de pagamento:
 em até 2x no cartão: preço formal
 3x ou mais no cartão: 20% de juros"""
 
-valor_produto = float(input("Digite o valor do produto: "))
+start = True
+start1 = True
 
-print("Escolha a forma de pagamento abaixo: ")
-print("------------------------------------------------")
-print("1 - À vista dinheiro/cheque: 10% de desconto;\n"
-      "2 - À vista no cartão: 5% de desconto;\n"
-      "3 - Em até 2x no cartão: preço formal;\n"
-      "4 - 3x ou mais no cartão: 20% de juros.\n"
-      "------------------------------------------------")
+while start:
 
-escolha = int(input("Escolha: "))
+    valor_produto = float(input("Digite o valor do produto: "))
 
-if escolha == 1:
-    valor_final = valor_produto - (valor_produto * 0.10)
-elif escolha == 2:
-    valor_final = valor_produto - (valor_produto * 0.05)
-elif escolha == 3:
-    valor_final = valor_produto
-elif escolha == 4:
-    valor_final = valor_produto + (valor_produto * 0.20)
-else:
-    valor_final = 0
-    print("Forma de pagamento inválida.")
+    print("Escolha a forma de pagamento abaixo: ")
+    print("------------------------------------------------")
+    print("1 - À vista dinheiro/cheque: 10% de desconto;\n"
+          "2 - À vista no cartão: 5% de desconto;\n"
+          "3 - Em até 2x no cartão: preço formal;\n"
+          "4 - 3x ou mais no cartão: 20% de juros.\n"
+          "------------------------------------------------")
 
-if valor_final != 0:
-    print(f"O valor final dessa compra é de: R${valor_final:.2f}")
+    escolha = int(input("Escolha: "))
 
+    if escolha == 1:
+        valor_final = valor_produto - (valor_produto * 0.10)
+    elif escolha == 2:
+        valor_final = valor_produto - (valor_produto * 0.05)
+    elif escolha == 3:
+        valor_final = valor_produto
+    elif escolha == 4:
+        valor_final = valor_produto + (valor_produto * 0.20)
+    else:
+        valor_final = 0
+        print("Forma de pagamento inválida.")
+
+    if valor_final != 0:
+        print(f"O valor final dessa compra é de: R${valor_final:.2f}")
+
+    while start1:
+        print("------------------------------------------------")
+        print("Deseja calcular outro produto?")
+        print("1 - Sim\n"
+              "2 - Não")
+        print("------------------------------------------------")
+        decisao = int(input("Sim ou não?: "))
+        print("------------------------------------------------")
+
+        while True:
+            if decisao != 1 and decisao != 2:
+                print("Opção inválida, Tente novamente!")
+                decisao = int(input("Sim ou não?: "))
+                break
+            else:
+                break
+
+        if decisao == 1:
+            break
+        elif decisao == 2:
+            start = False
+            break
+
+print('Volte sempre!')
